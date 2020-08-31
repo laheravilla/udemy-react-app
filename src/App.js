6ce78806970886247d2e20a5970aab2a134cde96
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-08-31 21:17:34 +0200

    understanding the children prop

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App!</h1>
+          <h1>Hi, I'm a React App!</h1>
           <Person name="Yurniel" age="39" />
-        <Person name="Emi" age="39">My hobbies: traveling</Person>
+          <Person name="Emi" age="39" >My hobbies: traveling</Person>
           <Person name="Gabriel" age="3" />
       </div>
     );
