ef2ab1cb6e8d16499778499de9ee1114f45df32d
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-01 05:49:09 +0200

    using the useState() hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+// Using useState approach when not extending Component
+const app = props => {
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            { name: 'Yurniel', age: 39 },
+            { name: 'Emi', age: 39 },
+            { name: 'Gabriel', age: 3 }
+        ],
+        otherState: 'Some other state'
+    });
+
+    // As switchNameHandler() does not take all setState data, we must include it manually,
+    // unlike when extending Component class
+    const [otherState, setOtherState] = useState('Some other value');
+
+    console.log(personsState, otherState);
+
+    const switchNameHandler = () => {
+        setPersonsState({
+            persons: [
+                { name: 'Yurniel  Lahera Villa', age: 39 },
+                { name: 'Emi Lahera Villa', age: 39 },
+                { name: 'Gabriel Lahera Villa', age: 3 }
+            ]
+        });
+    };
+
+    return (
+        <div className="App">
+            <h1>Hi, I'm a React App!</h1>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: traveling</Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+        </div>
+    );
+};
+
+export default app;
+
+
+/*
 class App extends Component {
+    // Only available when having extended Component
   state = {
     persons: [
       { name: 'Yurniel', age: 39 },
@@ -35,3 +79,4 @@ class App extends Component {
 }
 
 export default App;
+*/
