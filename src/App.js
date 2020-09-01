0d945ed5ae8614cd1b162749dd4724c85037bafb
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-01 06:47:13 +0200

    inline style

--- src/App.js
+++ src/App.js
@@ -34,11 +34,22 @@ class App extends Component {
     }
 
     render() {
+        const inlineStyle = {
+            backgroundColor: "white",
+            font: "inherit",
+            border: "1px solid blue",
+            padding: "8px",
+            cursor: "pointer"
+        };
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App!</h1>
                 {/*Ways of passing params to switchNameHandler(). Use .bind() as recommended way!*/}
-                <button onClick={this.switchNameHandler.bind(this, 'Yurniel Lahera')}>Switch Name</button>
+                <button
+                    style={inlineStyle}
+                    onClick={this.switchNameHandler.bind(this, 'Yurniel Lahera')}>Switch Name
+                </button>
                 {/*<button onClick={() => this.switchNameHandler('Yurniel!!')}>Switch Name</button>*/}
                 <Person
                     name={this.state.persons[0].name}
