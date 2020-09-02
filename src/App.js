66d154edb813dc8f4f35d68ffc9d4794313139c8
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-02 06:58:28 +0200

    handling dynamic content: the js way!

--- src/App.js
+++ src/App.js
@@ -52,6 +52,27 @@ class App extends Component {
             cursor: "pointer"
         };
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person
+                        name={this.state.persons[0].name}
+                        age={this.state.persons[0].age} />
+                    <Person
+                        name={this.state.persons[1].name}
+                        age={this.state.persons[1].age}
+                        click={this.switchNameHandler.bind(this, 'Yurniel!')}
+                        changed={this.nameChangedHandler}
+                    >My hobbies: traveling</Person>
+                    <Person
+                        name={this.state.persons[2].name}
+                        age={this.state.persons[2].age} />
+                </div>
+            );
+        }
+
         return (
             <div className="App">
                 <h1>Hi, I'm a React App!</h1>
@@ -61,25 +82,7 @@ class App extends Component {
                     onClick={this.togglePersonsHandler}>Switch Name
                 </button>
                 {/*<button onClick={() => this.switchNameHandler('Yurniel!!')}>Switch Name</button>*/}
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age} />
-                            <Person
-                                name={this.state.persons[1].name}
-                                age={this.state.persons[1].age}
-                                click={this.switchNameHandler.bind(this, 'Yurniel!')}
-                                changed={this.nameChangedHandler}
-                            >My hobbies: traveling</Person>
-                            <Person
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age} />
-                        </div>
-                        : null
-                }
-
+                {persons}
             </div>
         );
     }
