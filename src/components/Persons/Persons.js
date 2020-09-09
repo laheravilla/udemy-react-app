f1f3b736ee0d4bce465b9cc28d2b68e6119dc236
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-09 07:16:13 +0200

    PureComponent instead of shouldComponentUpdate()

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+// class Persons extends Component {
+/**
+ * React.PureComponent is similar to React.Component.
+ * The difference between them is that React.Component doesn’t implement shouldComponentUpdate(),
+ * but React.PureComponent implements it with a shallow prop and state comparison.
+ */
+class Persons extends PureComponent {
     // static getDerivedStateFromProps(props, state) {
     //     console.log('[Persons.js] getDerivedStateFromProps')
     //     return state;
     // }
 
+    /*
     shouldComponentUpdate(nextProps, nextState) {
         // Were forced to return true (if react must continue to update)
         // or false on the opposite case
         console.log('[Persons.js] shouldComponentUpdate');
 
-        return nextProps.persons !== this.props.persons;
+        return nextProps.persons !== this.props.persons ||
+            nextProps.changed !== this.props.changed ||
+            nextProps.click !== this.props.click;
     }
+     */
 
     /**
      *  Invoked RIGHT BEFORE the most recently rendered output is committed to e.g. the DOM.
