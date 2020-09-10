e44632c2b980da810195da485ac6be1156964a86
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-10 05:18:06 +0200

    rendering adjacent JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Person.css';
 import stlClasses from './Person.css'
+import Aux from '../../../hoc/Aux';
 
 /**
  * This is a stateless (or presentational) component, since it does not use useState
@@ -12,13 +13,31 @@ import stlClasses from './Person.css'
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
+        // return (
+        //     <div className={stlClasses.Person}>
+        //         <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+        //         <p>{this.props.children}</p>
+        //         <input type="text" value={this.props.name} onChange={this.props.changed}/>
+        //     </div>
+        // );
+
+        // We can also return an expression to avoid root element
         return (
-            <div className={stlClasses.Person}>
-                <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" value={this.props.name} onChange={this.props.changed}/>
-            </div>
+            <Aux>
+                <div className={stlClasses.Person}>
+                    <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
+                    <p>{this.props.children}</p>
+                    <input type="text" value={this.props.name} onChange={this.props.changed}/>
+                </div>
+            </Aux>
         );
+
+        // We can return an array of elements having their own unique keys, instead of a root element
+        // return [
+        //     <p key="p1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>,
+        //     <p key="p2">{this.props.children}</p>,
+        //     <input key="p3" type="text" value={this.props.name} onChange={this.props.changed}/>
+        // ];
     }
 }
 
