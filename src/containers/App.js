bb3f163588b1a7e6d40d367cc630c4f64debd02a
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-11 05:42:57 +0200

    passing unknown props

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,8 @@ class App extends Component {
                 { id: 3, name: 'Gabriel', age: 3 }
             ],
             showPersons: false,
-            showCockpit: true
+            showCockpit: true,
+            changeCounter: 0
         }
     }
 
@@ -88,7 +89,12 @@ class App extends Component {
         const persons = [...this.state.persons];
         persons[personIndex] = person;
 
-        this.setState({persons: persons})
+        this.setState((prevState, props) => {
+            return {
+                persons: persons,
+                changeCounter: prevState.changeCounter + 1
+            };
+        });
     }
 
     deletePersonHandler = (personIndex) => {
